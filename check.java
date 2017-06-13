31592dac3e65268cf523dc0f46b280f7f321d376
Monisha12 <monichandran12@gmail.com>
2017-06-13 18:39:09 +0530

    check.java

---
+++ check.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class check
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a>0)
+{
+System.out.println("the no is positive");
+}
+else if(a<0)
+{
+System.out.println("the no is negative);
+}
+else
+{
+System.out.println("the no is zero");
+}
+}
+}
