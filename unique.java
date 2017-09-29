a4ccb8b729576f554b8937753ab3ad3ae8b22576
Monisha12 <monichandran12@gmail.com>
2017-09-29 12:29:49 +0530

    unique.java

---
+++ unique.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class unique
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int arr[]=new int[n];
+for(int i=0;i<arr.length;i++)
+{
+arr[i]=s.nextInt();
+}
+for(
